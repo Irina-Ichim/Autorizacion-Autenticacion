c9f2763a8daa284c17e7e97513acf4c7333dc117
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-07-07 00:07:15 +0200

    creando login con jwt

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const expressJwt = ('express-jwt')
+const expressJwt = require('express-jwt')
 const User = require('./user')
 
 mongoose.connect('mongodb+srv://onlyirina7:IrinaDan2023@cluster0.pri7c4d.mongodb.net/auth?retryWrites=true&w=majority')
 const app= express()
 
 app.use(express.json())
+
+const signToken = _id => jwt.sign({_id }, 'secreto')
 
 app.post('/register', async (req, res) => {
     const { body} = req
@@ -21,14 +23,35 @@ app.post('/register', async (req, res) => {
         const salt = await bcrypt.genSalt()
         const hashed = await bcrypt.hash(body.password, salt)
         const user = await User.create({email: body.email, password: hashed, salt })
+        const signed = signToken(user._id)
 
-        res.send({_id: user._id })
+        res.status(201).send(signed)
 
     }catch(err) {
         console.log(err)
         res.status(500).send(err.message)
     }
 
+})
+app.post('/login', async (req, res) => {
+    const { body } = req
+    try {
+        const user = await User.findOne({ email: body.email})
+        if(!user) {
+            res.status(403).send('usuario y/o contraseña invalida')
+        } else {
+            const isMatch = await bcrypt.compare(body.password, user.password)
+            if(isMatch) {
+                const signed = signToken(user._id)
+                res.status(200).send(signed)
+
+            }else{
+                res.status(403).send('usuario y/o contraseña invalida')
+            }
+        }
+    }catch(err) {
+        res.status(500).send(err.message)
+    }
 })
 app.listen(3000, () => {
     console.log('listening in port 3000')
